02a966cbefba584dc5ef82a1361fec9b52795a3c
Forest Gregg <fgregg@uchicago.edu>
2021-04-18 13:01:04 -0400

    errors raised appropriately

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
+options(warn=2)
 load.lib<-c("rmarkdown", "knitr", "tidyverse", "tidycensus", "srvyr")
 install.lib<-load.lib[!load.lib %in% installed.packages()]
 for (lib in install.lib) install.packages(lib,dependencies=TRUE,repos="http://cran.us.r-project.org")
