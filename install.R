95a0643b03971c7741a9ae8b2502e61a4185c5ae
Forest Gregg <fgregg@uchicago.edu>
2021-04-19 20:54:07 -0400

    add lwgeom to dependencies

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 options(warn=2)
-load.lib<-c("rmarkdown", "knitr", "tidyverse", "tidycensus", "srvyr")
+load.lib<-c("rmarkdown", "knitr", "tidyverse", "tidycensus", "srvyr", "lwgeom")
 install.lib<-load.lib[!load.lib %in% installed.packages()]
 for (lib in install.lib) install.packages(lib,dependencies=TRUE,repos="http://cran.us.r-project.org")
